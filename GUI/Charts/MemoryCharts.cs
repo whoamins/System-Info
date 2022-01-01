c5dd57627331eff27107646c44a49f78dc8603ce
whoamins <40am1ns@protonmail.com>
2022-01-01 19:46:30 +0300

    feat: WPF GUI

---
+++ GUI/Charts/MemoryCharts.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using ConsoleApp1;
+using LiveCharts;
+using LiveCharts.Defaults;
+using LiveCharts.Wpf;
+
+namespace GUI.Charts;
+
+public class MemoryCharts
+{
+    private readonly Dictionary<string, List<string>> _memoryUsageInfo = MemoryInfo.MemoryUsageInfo();
+    
+    public SeriesCollection GetTotalMemoryChart()
+    {
+        var seriesCollection = new SeriesCollection();
+        
+        foreach (var info in _memoryUsageInfo)
+        {
+            var title = info.Key;
+            var totalSpace = 0.0;
+
+            if (double.TryParse(info.Value[0], out var x))
+            {
+                totalSpace = x;
+            }
+
+            seriesCollection.Add(new PieSeries
+            {
+                Title = title,
+                Values = new ChartValues<ObservableValue> {new ObservableValue(totalSpace)},
+                DataLabels = true
+            });
+        }
+
+        return seriesCollection;
+    }
+}
