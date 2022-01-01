dd13b72af62503b30a3d35e0cb97968561ea395b
whoamins <40am1ns@protonmail.com>
2022-01-01 19:44:45 +0300

    Initial commit

---
+++ Core/MemoryInfo.cs
@@ -0,0 +1,39 @@
+﻿namespace ConsoleApp1;
+
+public class MemoryInfo
+{
+    public static Dictionary<string, List<string>> MemoryUsageInfo()
+    {
+        var drives = DriveInfo.GetDrives();
+        var info = new Dictionary<string, List<string>>();
+
+        foreach (var drive in drives)
+        {
+            var values = new List<string>
+            {
+                $"{drive.TotalSize / Math.Pow(1024, 3):F2}",
+                $"{drive.AvailableFreeSpace / Math.Pow(1024, 3):F2}"
+            };
+            
+            info.Add(drive.Name, values);
+        }
+
+        return info;
+    }
+
+    internal static void ShowMemoryUsage()
+    {
+        var info = MemoryUsageInfo();
+
+        foreach (var values in info)
+        {
+            Console.WriteLine(values.Key);
+
+            foreach (var value in values.Value)
+            {
+                if (double.TryParse(value, out var x))
+                    Console.WriteLine($"\t{x}");
+            }
+        }
+    }
+}
---
+++ Core/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
