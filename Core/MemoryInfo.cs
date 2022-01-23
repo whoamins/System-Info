262fa09eb8810e9dd45406ba3cf07fa126f2d515
whoamins <40am1ns@protonmail.com>
2022-01-23 19:33:42 +0300

    Some Disk Info

--- Core/MemoryInfo.cs
+++ Core/MemoryInfo.cs
@@ -1,4 +1,5 @@
-﻿namespace ConsoleApp1;
+﻿
+namespace ConsoleApp1;
 
 public class MemoryInfo
 {
@@ -14,5 +15,11 @@ public class MemoryInfo
     {
         return _driveInfo.ToDictionary<DriveInfo?, string, double>(drive => drive.Name,
             drive => Math.Round(drive.TotalSize / Math.Pow(1024, 3)));
+    }
+
+    public Dictionary<string, string> GetDriveFormat()
+    {
+        return _driveInfo.ToDictionary<DriveInfo?, string, string>(drive => drive.Name,
+            drive => drive.DriveFormat);
     }
 }
--- Core/Program.cs
+++ Core/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-
+﻿
 namespace ConsoleApp1
 {
     class Program
